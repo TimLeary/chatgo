5d1121aa3bd554d7bc6b68babbaee1d5d14efdf4
Nasztanovics Dávid <dnasztanovics@gmail.com>
2017-11-20 17:11:24 +0100

    start creating login

--- main/main.go
+++ main/main.go
@@ -9,6 +9,7 @@ import (
 	"flag"
 	"os"
 	"chatgo/trace"
+	"strings"
 	"fmt"
 )
 
@@ -29,12 +30,30 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r  *http.Request) {
 	t.templ.Execute(w, r)
 }
 
+// loginHandler handles the third-party login process.
+// format: /auth/{action}/{provider}
+func loginHandler(w http.ResponseWriter, r *http.Request)  {
+	segs := strings.Split(r.URL.Path, "/")
+	action := segs[2]
+	provider := segs[3]
+
+	switch action {
+	case "login":
+		log.Println("TODO handle login for", provider)
+	default:
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprintf(w, "Auth action %s not supported", action)
+	}
+}
+
 func main() {
 	var addr = flag.String("addr", ":8888", "The addr of the  application.")
 	flag.Parse() // parse the flags
 	r := newRoom()
 	r.tracer = trace.New(os.Stdout)
 	http.Handle("/chat", MustAuth(&templateHandler{filename: "chat.html"}))
+	http.Handle("/login", &templateHandler{filename: "login.html"})
+	http.HandleFunc("/auth/", loginHandler)
 	http.Handle("/room", r)
 	pwd, _ := os.Getwd()
 	assets := pwd + "/../assets/"
@@ -47,11 +66,4 @@ func main() {
 	if err := http.ListenAndServe(*addr, nil); err != nil {
 		log.Fatal("ListenAndServe:", err)
 	}
-}
-
-func exists(path string) (bool, error) {
-	_, err := os.Stat(path)
-	if err == nil { return true, nil }
-	if os.IsNotExist(err) { return false, nil }
-	return true, err
 }
